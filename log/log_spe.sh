2eefbf2bed1051acbc0f26d0900a60047366c80e
wjcgithub <wjc163@sina.cn>
2015-03-18 21:06:37 +0800

    数据入库

--- log/log_spe.sh
+++ log/log_spe.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 #log_spe.sh
+
+mysql="/usr/local/mysql/bin/mysql -h10.1.1.237 -udemo -p123456"
+
+sql="show databases"
+
+# create_sql="create table test.countab(
+# 		id int unsigned auto_increment primary key,
+# 		date varchar(100),
+# 		ip varchar(100),
+# 		num int
+# 	)"
+
+$mysql -e "$create_sql"
+
 yesterday=`date -d yesterday +%Y%m%d`
 
 srclog="/usr/local/apache2/logs/access_log"
@@ -13,4 +27,4 @@ pkill -1 httpd
 
 
 #统计,然后入库
-#cat /usr/local/apache2/logsbak/access_20150317.log | awk '{print $1}' | sort -R | uniq -c
+cat /usr/local/apache2/logsbak/access_20150317.log | awk '{print $1}' | sort -R | uniq -c 
